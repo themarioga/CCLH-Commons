be42f44b549a12731979a9e6681b34ab4718a831
MARIOGONZALEZALVARAD <marioga2512@gmail.com>
2024-01-13 17:16:14 +0100

    Configuration improvements

---
+++ src/main/resources/db/migration/V1/V1.0/V1.0.0/V1.0.0_5__Bot_configuration.sql
@@ -0,0 +1,13 @@
+-- v1.0.0_5 - Bot data
+
+INSERT INTO t_configuration
+VALUES  ('cclh_bot_name', 'Cartas contra la humanidad'),
+        ('cclh_bot_alias', '@cclhbot'),
+        ('cclh_bot_version', '1.0.0'),
+        ('cclh_bot_owner_id', ''),
+        ('cclh_bot_owner_alias', '@themarioga'),
+        ('dictionaries_bot_name', 'Diccionarios para CCLHBot'),
+        ('dictionaries_bot_alias', '@cclhdictionariesbot'),
+        ('dictionaries_bot_version', '1.0.0'),
+        ('dictionaries_bot_owner_id', ''),
+        ('dictionaries_bot_owner_alias', '@themarioga');
