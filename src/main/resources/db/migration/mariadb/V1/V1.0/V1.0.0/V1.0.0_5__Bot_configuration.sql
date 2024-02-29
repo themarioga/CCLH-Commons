3b5e22abc30d50b701cb3e47fc63b8912e60130f
MARIOGONZALEZALVARAD <marioga2512@gmail.com>
2024-02-29 23:32:11 +0100

    Help message

--- src/main/resources/db/migration/mariadb/V1/V1.0/V1.0.0/V1.0.0_5__Bot_configuration.sql
+++ src/main/resources/db/migration/mariadb/V1/V1.0/V1.0.0/V1.0.0_5__Bot_configuration.sql
@@ -6,8 +6,10 @@ VALUES  ('cclh_bot_name', 'Cartas contra la humanidad'),
         ('cclh_bot_version', '1.0.0'),
         ('cclh_bot_owner_id', ''),
         ('cclh_bot_owner_alias', '@themarioga'),
+        ('cclh_bot_help_url', 'https://telegra.ph/Manual-del-bot-Cartas-Contra-la-Humanidad-cclhbot-01-31'),
         ('dictionaries_bot_name', 'Diccionarios para CCLHBot'),
         ('dictionaries_bot_alias', '@cclhdictionariesbot'),
         ('dictionaries_bot_version', '1.0.0'),
         ('dictionaries_bot_owner_id', ''),
-        ('dictionaries_bot_owner_alias', '@themarioga');
+        ('dictionaries_bot_owner_alias', '@themarioga'),
+        ('dictionaries_bot_help_url', 'https://telegra.ph/Manual-del-bot-Cartas-Contra-la-Humanidad-cclhbot-01-31');
