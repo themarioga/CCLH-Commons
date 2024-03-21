20cba9f34d06db90377abc87ae48b64917c55912
MARIOGONZALEZALVARAD <marioga2512@gmail.com>
2024-03-22 00:09:46 +0100

    Modified migrations for dictionaries config

---
+++ src/main/resources/db/migration/mariadb/V1/V1.0/V1.1.0/V1.1.0_2__DictionariesConfiguration.sql
@@ -0,0 +1,10 @@
+-- v1.1.0_2 - Dictionaries configuration
+
+INSERT INTO t_configuration
+VALUES ('dictionaries_min_whitecards', '405'),
+       ('dictionaries_min_blackcards', '50'),
+       ('dictionaries_max_whitecards', '600'),
+       ('dictionaries_max_blackcards', '200'),
+       ('dictionaries_max_whitecard_length', '90'),
+       ('dictionaries_max_blackcard_length', '130'),
+       ('dictionaries_max_collaborators', '10');
